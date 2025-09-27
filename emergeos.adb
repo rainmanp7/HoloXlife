2033191beaa9872e3683180950340d6a714c8481
Area15 R&D <muslimsoap@gmail.com>
2025-09-28 06:09:14 +0800

    Update emergeos.adb

--- emergeos.adb
+++ emergeos.adb
@@ -1,11 +1,9 @@
--- emergeos.adb
 with Ada.Text_IO; use Ada.Text_IO;
 with System.Memory;
 
 procedure EmergeOS is
    pragma Export (C, EmergeOS, "_Ada_Main");
 
-   -- Test heap allocation
    type Int_Ptr is access Integer;
    X : Int_Ptr;
 begin
@@ -20,7 +18,6 @@ begin
       Put_Line ("[Ada] Heap allocation FAILED!");
    end if;
 
-   -- Intentional infinite loop (no exit in kernel)
    loop
       null;
    end loop;
