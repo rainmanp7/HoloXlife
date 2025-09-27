b0790249959256e5eafbce389c1bd94c692aa0da
Area15 R&D <muslimsoap@gmail.com>
2025-09-28 05:58:08 +0800

    Update s-lastch.adb

--- s-lastch.adb
+++ s-lastch.adb
@@ -1,8 +1,11 @@
 -- s-lastch.adb
+with Ada.Text_IO; use Ada.Text_IO;
 package body System.Last_Chance_Handler is
    procedure Last_Chance_Handler (Msg : String; Line : Integer) is
    begin
-      -- Halt on unhandled exception
+      Put_Line ("[EXCEPTION] Unhandled exception: " & Msg);
+      Put_Line ("[EXCEPTION] Line: " & Integer'Image (Line));
+      -- Halt
       loop
          null;
       end loop;
