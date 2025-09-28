cdda9b72885f3121e2cc60eb215f9f6d0fe22da8
Area15 R&D <muslimsoap@gmail.com>
2025-09-28 19:52:11 +0800

    Update boot.ads

--- boot.ads
+++ boot.ads
@@ -1,7 +1,4 @@
--- boot.ads - HoloXlife Bootloader Specification
-package Boot is
-   pragma Export (Assembly, Boot, "boot_main");
-   pragma No_Return (Boot);
-   
-   procedure Boot;
-end Boot;
+-- boot.ads
+procedure Boot;
+pragma Export (Assembly, Boot, "boot_main");
+pragma No_Return (Boot);
