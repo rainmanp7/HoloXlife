5b74b5d61fb5d7a9a933b4365f278b701bfef43c
Area15 R&D <muslimsoap@gmail.com>
2025-09-29 06:42:52 +0800

    Delete emergeos.ads

--- emergeos.ads
+++
@@ -1,6 +0,0 @@
--- emergeos.ads - HoloXlife OS Specification
-package EmergeOS is
-   procedure EmergeOS;
-   pragma Export (Assembly, EmergeOS, "emergeos_main");
-   pragma No_Return (EmergeOS);
-end EmergeOS;
