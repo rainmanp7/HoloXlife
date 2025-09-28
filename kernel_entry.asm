61ea457f319d89ce161f4ef3b7f027c69124c74b
Area15 R&D <muslimsoap@gmail.com>
2025-09-28 08:02:34 +0800

    Update kernel_entry.asm

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,7 +1,29 @@
-; kernel_entry.asm - Pure Ada OS entry point
+; kernel_entry.asm - Pure Ada OS entry point with port I/O functions
 [bits 32]
 extern emergeos_main    ; Ada procedure name
 global _start
+global port_out_8       ; Export port I/O functions for Ada
+global port_in_8
+
+; Port output function (for Ada)
+port_out_8:
+    push ebp
+    mov ebp, esp
+    mov dx, [ebp+8]     ; port
+    mov al, [ebp+12]    ; value
+    out dx, al
+    pop ebp
+    ret
+
+; Port input function (for Ada)  
+port_in_8:
+    push ebp
+    mov ebp, esp
+    mov dx, [ebp+8]     ; port
+    xor eax, eax
+    in al, dx
+    pop ebp
+    ret
 
 _start:
     ; Set up stack for Ada
